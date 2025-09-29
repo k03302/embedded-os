103722887f542157b5c44a1d10bb45efceabd161
k03302 <unisonman660@gmail.com>
2025-09-29 18:41:16 +0900

    Add memory settings

---
+++ include/ARMv7AR.h
@@ -0,0 +1,8 @@
+#define ARM_MODE_BIT_USR 0x10  // User mode
+#define ARM_MODE_BIT_FIQ 0x11  // Fast Interrupt mode  
+#define ARM_MODE_BIT_IRQ 0x12  // Interrupt mode
+#define ARM_MODE_BIT_SVC 0x13  // Supervisor mode
+#define ARM_MODE_BIT_ABT 0x17  // Abort mode
+#define ARM_MODE_BIT_UND 0x1B  // Undefined mode
+#define ARM_MODE_BIT_SYS 0x1F  // System mode  
+#define ARM_MODE_BIT_MON 0x16  // Monitor mode
