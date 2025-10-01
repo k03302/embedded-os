e03bd8113cc7ca910471c30b8de61973f50bcb32
k03302 <unisonman660@gmail.com>
2025-10-01 15:36:45 +0900

    Change interrupt interface function name

--- hal/rvpb/Interrupt.c
+++ hal/rvpb/Interrupt.c
@@ -59,7 +59,7 @@ void Hal_interrupt_disable(uint32_t interrupt_num)
         CLR_BIT(GicDist->setenable2, bit_num);
     }
 }
-void Hal_register_handler(InterHdlr_fptr handler, uint32_t interrupt_num)
+void Hal_interrupt_register_handler(InterHdlr_fptr handler, uint32_t interrupt_num)
 {
     sHandlers[interrupt_num] = handler;
 }
