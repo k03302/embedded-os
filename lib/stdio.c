4f52f592b293fe049af6aac3c3b7e5350bbdce0e
k03302 <unisonman660@gmail.com>
2025-10-02 15:57:54 +0900

    Add debug_printf to stdio

--- lib/stdio.c
+++ lib/stdio.c
@@ -1,6 +1,11 @@
 #include "stdint.h"
 #include "HalUart.h"
 #include "stdio.h"
+#include "stdarg.h"
+
+#define PRINTF_BUF_LEN 1024
+
+static char printf_buf[PRINTF_BUF_LEN]; // 1KB
 
 uint32_t putstr(const char *s)
 {
@@ -11,4 +16,97 @@ uint32_t putstr(const char *s)
         c++;
     }
     return c;
+}
+
+uint32_t utoa(char *buf, uint32_t val, utoa_t base)
+{
+    uint32_t c = 0;
+    int32_t idx = 0;
+    char tmp[11]; // It is big enough for store 32 bit int
+
+    do
+    {
+        uint32_t t = val % (uint32_t)base;
+        if (t >= 10)
+        {
+            t += 'A' - '0' - 10;
+        }
+        tmp[idx] = (t + '0');
+        val /= base;
+        idx++;
+    } while (val);
+
+    // reverse
+    idx--;
+    while (idx >= 0)
+    {
+        buf[c++] = tmp[idx];
+        idx--;
+    }
+
+    return c;
+}
+
+uint32_t vsprintf(char *buf, const char *format, va_list arg)
+{
+    char *format_ptr = format;
+    char *buf_ptr = buf;
+    char *str = NULL;
+    uint32_t uint;
+
+    while (*format_ptr != '\0')
+    {
+        if (*format_ptr == '%')
+        {
+            format_ptr++;
+            switch (*format_ptr)
+            {
+            case 'c':
+                *buf_ptr = (char)va_arg(arg, int32_t);
+                buf_ptr++;
+                break;
+
+            case 's':
+                str = (char *)va_arg(arg, char *);
+                if (str == NULL)
+                {
+                    str = "(null)";
+                }
+                while (*str != '\0')
+                {
+                    *buf_ptr = *str;
+                    buf_ptr++;
+                    str++;
+                }
+                break;
+
+            case 'u':
+                uint = (uint32_t)va_arg(arg, uint32_t);
+                buf_ptr += utoa(buf_ptr, uint, utoa_dec);
+                break;
+
+            case 'x':
+                uint = (uint32_t)va_arg(arg, uint32_t);
+                buf_ptr += utoa(buf_ptr, uint, utoa_hex);
+                break;
+            }
+            format_ptr++;
+        }
+        else
+        {
+            *buf_ptr = *format_ptr;
+            buf_ptr++;
+            format_ptr++;
+        }
+    }
+}
+
+uint32_t debug_printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vsprintf(printf_buf, format, args);
+    va_end(args);
+
+    return putstr(printf_buf);
 }
--- lib/stdio.h
+++ lib/stdio.h
@@ -1,6 +1,16 @@
 #ifndef LIB_STDIO_H_
 #define LIB_STDIO_H_
 
+#include "stdarg.h"
+
+typedef enum utoa_t
+{
+    utoa_dec = 10,
+    utoa_hex = 16,
+} utoa_t;
+
 uint32_t putstr(const char *s);
+uint32_t debug_printf(const char *format, ...);
+uint32_t vsprintf(char *buf, const char *format, va_list arg);
 
 #endif
