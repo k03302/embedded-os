0ee18949a573e3034a474486ca911f864e5c6b31
k03302 <unisonman660@gmail.com>
2025-10-02 20:32:56 +0900

    Add tests

--- boot/Main.c
+++ boot/Main.c
@@ -1,21 +1,19 @@
 #include "stdint.h"
 #include "HalUart.h"
 #include "HalInterrupt.h"
+#include "HalTimer.h"
+#include "stdlib.h"
 #include "stdio.h"
 
 static void Hw_init(void);
+static void Printf_test(void);
+static void Timer_test(void);
 
 int main()
 {
     Hw_init();
-    uint32_t i = 100;
 
-    putstr("Hello World!\n");
-
-    while (1)
-    {
-        __asm__ volatile("WFI"); // Wait for interrupt
-    }
+    Timer_test();
 
     return 0;
 }
@@ -24,4 +22,21 @@ void Hw_init(void)
 {
     Hal_interrupt_init();
     Hal_uart_init();
+}
+
+void Printf_test(void)
+{
+    while (1)
+    {
+        __asm__ volatile("WFI"); // Wait for interrupt
+    }
+}
+
+void Timer_test(void)
+{
+    while (1)
+    {
+        debug_printf("current 1ms count: %u\n", Hal_timer_get_1ms_counter());
+        delay(1000);
+    }
 }
