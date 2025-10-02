67d7952aac736c48c453d951082b6261b47d0310
k03302 <unisonman660@gmail.com>
2025-10-03 01:17:10 +0900

    Integrate kernel task system into build and main

--- boot/Main.c
+++ boot/Main.c
@@ -2,12 +2,17 @@
 #include "HalUart.h"
 #include "HalInterrupt.h"
 #include "HalTimer.h"
+#include "task.h"
 #include "stdlib.h"
 #include "stdio.h"
 
 static void Hw_init(void);
 static void Printf_test(void);
 static void Timer_test(void);
+static void Kernel_init(void);
+static void User_task0(void);
+static void User_task1(void);
+static void User_task2(void);
 
 int main()
 {
@@ -40,4 +45,44 @@ void Timer_test(void)
         debug_printf("current 1ms count: %u\n", Hal_timer_get_1ms_counter());
         delay(1000);
     }
+}
+
+void Kernel_init(void)
+{
+    uint32_t taskId;
+
+    Kernel_task_init();
+
+    taskId = Kernel_task_create(User_task0);
+    if (taskId == -1)
+    {
+        putstr("Task0 create failed\n");
+    }
+
+    taskId = Kernel_task_create(User_task1);
+    if (taskId == -1)
+    {
+        putstr("Task1 create failed\n");
+    }
+
+    taskId = Kernel_task_create(User_task2);
+    if (taskId == -1)
+    {
+        putstr("Task2 create failed\n");
+    }
+}
+
+void User_task0(void)
+{
+    putstr("User Task0\n");
+}
+
+void User_task1(void)
+{
+    putstr("User Task1\n");
+}
+
+void User_task2(void)
+{
+    putstr("User Task2\n");
 }
