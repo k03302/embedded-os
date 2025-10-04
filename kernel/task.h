811cad68cb45cdb2e3d36069c4364970d7a631b0
k03302 <unisonman660@gmail.com>
2025-10-05 01:22:24 +0900

    docs: Add function descriptions

--- kernel/task.h
+++ kernel/task.h
@@ -23,7 +23,36 @@ typedef struct KernelTcb_t
 
 typedef void (*KernelTaskFunc_t)(void);
 
+/*
+Initialize kernel stack & context
+Each task's stack size is fixed as USR_TASK_STACK_SIZE
+
+From top to bottom, each stack is composed of:
+- padding (4byte)
+- task context
+- stack data
+- sp (top of stack)
+- empty memory
+- stack_base (0)
+*/
 void Kernel_task_init(void);
+
+/*
+Create a new task with given start function
+Returns task ID on success, -1 if task limit exceeded
+*/
 uint32_t Kernel_task_create(KernelTaskFunc_t startFunc, uint32_t priority);
+
+/*
+Perform context switch to next task
+
+1. Backup current task's context
+2. Get next task's control block
+3. Read sp from control block
+4. Reconstruct context from next task's stack
+5. Set pc to next execution position
+
+*/
+void Kernel_task_schedule(void);
 
 #endif
