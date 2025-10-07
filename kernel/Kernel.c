1f8b23a63e4bb0ab522ca0e723e04911f62b0581
k03302 <unisonman660@gmail.com>
2025-10-07 16:16:46 +0900

    feat: add kernel event system

--- kernel/Kernel.c
+++ kernel/Kernel.c
@@ -1,4 +1,5 @@
 #include "Kernel.h"
+#include "memio.h"
 
 void Kernel_yield(void)
 {
@@ -8,4 +9,34 @@ void Kernel_yield(void)
 void Kernel_start(void)
 {
     Kernel_task_start();
+}
+
+void Kernel_send_events(uint32_t event_list)
+{
+    for (uint32_t i = 0; i < 32; i++)
+    {
+        if ((event_list >> i) & 1)
+        {
+            Kernel_event_flag_set((KernelEventFlag_t)(1 << i));
+        }
+    }
+}
+
+KernelEventFlag_t Kernel_wait_events(uint32_t event_list)
+{
+    for (uint32_t i = 0; i < 32; i++)
+    {
+        if ((event_list >> i) & 1)
+        {
+            KernelEventFlag_t waiting_event = KernelEventFlag_Empty;
+            waiting_event = (KernelEventFlag_t)SET_BIT(waiting_event, i);
+
+            if (Kernel_event_flag_check(waiting_event))
+            {
+                return waiting_event;
+            }
+        }
+    }
+
+    return KernelEventFlag_Empty;
 }
